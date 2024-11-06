089bf9abd6600dc0b54d3631d3b26ad8e416c4f0
AkhmadKholmurodov <seehyuk2000@gmail.com>
2024-11-06 15:51:40 +0900

    first commit

---
+++ easy1.py
@@ -0,0 +1,12 @@
+/* Berilgan raqamlarni teskarichasiga yozib ekranga chiqarish
+Misol uchun 154, 2024  sonini
+*/
+
+def print_digits(num: int) -> None:
+	/*  154 % 10 = 4;
+	154 / 10 = 15;
+	15 % 10 = 5;
+	15 / 10 = 1;
+	1 % 10 = 1;
+	1 / 10 = 1; */
+
