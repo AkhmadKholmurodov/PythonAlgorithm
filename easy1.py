a0938bed5b00a7b269dbc359d5d36d0a8a57fb08
AkhmadKholmurodov <seehyuk2000@gmail.com>
2024-11-09 11:57:18 +0900

    update

--- easy1.py
+++ easy1.py
@@ -1,12 +1,19 @@
-/* Berilgan raqamlarni teskarichasiga yozib ekranga chiqarish
-Misol uchun 154, 2024  sonini
-*/
+#  Berilgan raqamlarni teskarichasiga yozib ekranga chiqarish
+#  Misol uchun 154, 2024  sonini
 
 def print_digits(num: int) -> None:
-	/*  154 % 10 = 4;
-	154 / 10 = 15;
-	15 % 10 = 5;
-	15 / 10 = 1;
-	1 % 10 = 1;
-	1 / 10 = 1; */
+    if num == 0:
+        print("0")
+        return
 
+    while num != 0:
+        digit = num % 10
+        print(digit, end=" ")
+        num //= 10
+		# 154 % 10 = 4;
+		# 154 / 10 = 15;
+		# 15 % 10 = 5;
+
+		# 15 / 10 = 1;
+		# 1 % 10 = 1;
+		# 1 / 10 = 1; 
