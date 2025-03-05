52604c9118ffc18f40c7380c15bbb1c7780d5981
Akhmad Kholmurodov <87185085+AkhmadKholmurodov@users.noreply.github.com>
2025-03-05 09:33:46 -0500

    Update PalindromeNumbers.py

--- PalindromeNumbers.py
+++ PalindromeNumbers.py
@@ -7,3 +7,5 @@ class Solution:
             revertedNumber = revertedNumber * 10 + x % 10 # x가 0에서 값이 많을떄  revertedNumber 곱하기 10 + x를 10에 나눌때 남는 값
             x //= 10  #마지막 숫자를 없애기 
         return x == revertedNumber or x == revertedNumber // 10
+result = Solution().isPalindrome(33333)
+print(result)
